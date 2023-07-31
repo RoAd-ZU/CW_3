179dec51e222853bf6c3033016d1cb888d13db8c
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-07-31 23:10:38 +0300

    load_jason full utills full main full

--- src/main.py
+++ src/main.py
@@ -2,3 +2,16 @@
 
 from utills import hide_symbols
 
+def start_main():
+    for element in hide_symbols():
+        print(f"{element['date']} {element['description']}")
+        if 'from' in element:
+            print(f"{element['from']} -> {element['to']}")
+        else:
+            print(element['to'])
+        print(f"{element['operationAmount']['amount']} {element['operationAmount']['currency']['name']}")
+        print()
+
+
+if __name__ == "__main__":
+    start_main()
