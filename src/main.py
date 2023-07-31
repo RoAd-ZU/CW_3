c153eb966c6ec5f577e1f7192947798f203993b2
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-07-31 22:55:17 +0300

    load_jason full utills full start main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+
+from utills import hide_symbols
+
--- src/utills.py
+++ src/utills.py
@@ -4,8 +4,31 @@ from load_json import date_correction, sorting
 
 date_cor = date_correction()
 
-def tes_load_json():
-    print(sorting(date_cor))
+def get_five_operations():
+    return sorting(date_cor)[-5:]
+
+def reversed_operation_five():
+    r_operation_five = get_five_operations()
+    r_operation_five.reverse()
+    return r_operation_five
+
+def hide_symbols():
+    five_operations = reversed_operation_five()
+
+    for element in five_operations:
+        if element['to'][0] == 'С':
+            element['to'] ='Счет ' + '**' + element['to'][-4:]
+        else:
+            element['to'] = element['to'][:-12] + ' ' + element['to'][-11:-9] + '** ' + '**** ' + element['to'][-4:]
+        if 'from' in element:
+            if element['from'][0] == 'С':
+                element['from'] = 'Счет ' + '**' + element['from'][-4:]
+            else:
+                element['from'] = element['from'][:-12] + ' ' + element['from'][-11:-9] + '** ' + '**** ' + element['from'][-4:]
+        else:
+            continue
 
 
-tes_load_json()
+    return five_operations
+
+
